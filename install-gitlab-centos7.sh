ae6ddca563d0ef6ae12c89bfe54e0e5064036318
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-11-06 15:27:47 +0800

    Create install-gitlab-centos7.sh

---
+++ install-gitlab-centos7.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo yum install -y firewalld curl policycoreutils-python openssh-server
+sudo systemctl enable sshd
+sudo systemctl start sshd
+sudo firewall-cmd --permanent --add-service=http
+sudo systemctl reload firewalld
+
+# sudo yum install postfix
+# sudo systemctl enable postfix
+# sudo systemctl start postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+# EXTERNAL_URL="http://gitlab.example.com"
+sudo EXTERNAL_URL="$1" yum install -y gitlab-ee
